29063e03e3b7c1c8d1fde06b5bd5a6519da82e97
bonochof <ryos427@yahoo.co.jp>
2016-09-10 20:33:21 +0900

    restaurants controllerを作る

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
-Rails.application.routes.draw do
+﻿Rails.application.routes.draw do
   devise_for :drinkers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :terms, only: [:show]
   get :signup, to: "devise/registrations#new"
   resources :festivals, only: [:index, :show]
-  resources :restaurants, only: [:index, :show] do
-    resources :sakes, only: [:index]
-    resources :checkins, only: [:index, :create]
-  end
+#  resources :restaurants, only: [:index, :show] do
+#    resources :sakes, only: [:index]
+#  end
+  resources :checkins, only: [:index, :create]
   resources :drinkings, only: [:index, :new, :create, :delete]
   get :itsitoolate, to: "festivals#itstoolate"
   get :thewinner, to: "festivals#thewinner"
-  get 'checkin/index'=>'checkin#index' 
+  get 'restaurants/checkins', to: 'checkin#index'
 end
